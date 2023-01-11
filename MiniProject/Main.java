88ccf36f1ca9da176bc294fa0811c4a1b246027a
hyunwoocastle <112748454+hyunwoocastle@users.noreply.github.com>
2023-01-11 16:09:19 +0900

    Complete MiniPrj Commit.

---
+++ MiniProject/Main.java
@@ -0,0 +1,14 @@
+package my.a.first.prj;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		TotalCtrll totalctrll = new TotalCtrll();
+		
+		totalctrll.startMenu();
+		
+	}
+
+}
